addd332a2a9faffd5e04f49989200427345e3857
EliasMena <eliasmenapech@gmail.com>
2023-12-17 15:26:46 -0600

    Refactorizacion de la clase RickAndMortyReposityImp

--- app/src/main/java/com/devdroid/rickandmortyapp/data/repository/RickAndMortyReposityImp.kt
+++ app/src/main/java/com/devdroid/rickandmortyapp/data/repository/RickAndMortyReposityImp.kt
@@ -5,6 +5,8 @@ import com.devdroid.rickandmortyapp.data.local.entities.CharacterLocationaAndOri
 import com.devdroid.rickandmortyapp.data.local.entities.toDatabase
 import com.devdroid.rickandmortyapp.data.remote.api.RickAndMortyApiClient
 import com.devdroid.rickandmortyapp.domain.model.CharacterItem
+import com.devdroid.rickandmortyapp.domain.model.LocationItem
+import com.devdroid.rickandmortyapp.domain.model.OriginItem
 import com.devdroid.rickandmortyapp.domain.model.toDomain
 import com.devdroid.rickandmortyapp.domain.repository.RickAndMortyRepository
 import javax.inject.Inject
@@ -13,74 +15,43 @@ class RickAndMortyReposityImp @Inject constructor(
     private val api: RickAndMortyApiClient,
     private val dao: CharacterDao
 ) : RickAndMortyRepository{
-    override suspend fun getCharacterOffline(): List<CharacterItem> {
+    override suspend fun getCharacterOffline(): List<CharacterItem>{
         if (dao.countCharacter() == 0){
-            val listCharactersAndLocation = mutableListOf<CharacterLocationaAndOrigin>()
-            api.obtenerCharacter().body()?.characters?.forEach {
-                listCharactersAndLocation.add(
-                    CharacterLocationaAndOrigin(
-                        it.toDatabase(),
-                        it.location!!.toDatabase(),
-                        it.origin!!.toDatabase()
-                    )
+            api.obtenerCharacter().body()?.characters?.map {
+                CharacterLocationaAndOrigin(
+                    it.toDatabase(),
+                    it.location!!.toDatabase(),
+                    it.origin!!.toDatabase()
                 )
+            }?.let {
+                listCharacterLocationAndOrigin ->
+                dao.insertCharactersLocationAndOrigin(listCharacterLocationAndOrigin)
             }
-
-            dao.insertCharactersLocationAndOrigin(listCharactersAndLocation)
-
         }
 
-
-
-        //Obtiene todos los Characters
-
-        val listCharacters = mutableListOf<CharacterItem>()
-
-        dao.getAllCharacterAndLocation().forEach {
-            val entity = it.character.toDomain()
-            entity.origin = it.origin.toDomain()
-            entity.location = it.location.toDomain()
-            listCharacters.add(entity)
-        }
-        return listCharacters
-    }
-
-    //Este se utitli
-    override suspend fun getUrlCharacters(): List<CharacterItem> {
-        val response = api.obtenerCharacter()
-
-        return if(response.isSuccessful){
-            response.body()?.characters?.map {
-                it.toDomain()
-            }?: emptyList()
-        }else{
-            throw Exception(response.errorBody()?.string())
+        return dao.getAllCharacterAndLocation().map {
+            it.character.toDomain().apply {
+                origin = it.origin.toDomain()
+                location = it.location.toDomain()
+            }
         }
     }
 
-    override suspend fun getCharacterById(id: Int): CharacterItem {
 
-        val characterLocationAndOrigin = dao.getCharacterById(id)
+    override suspend fun getUrlCharacters(): List<CharacterItem>  = api.obtenerCharacter().takeIf{response -> response.isSuccessful}?.let{ it.body()?.characters?.map { it.toDomain() }?: emptyList() }?: throw Exception("Error al obtener los datos")
 
-        return characterLocationAndOrigin.character.toDomain().apply {
-            origin = characterLocationAndOrigin.origin.toDomain()
-            location = characterLocationAndOrigin.location.toDomain()
-        }
 
+    override suspend fun getCharacterById(id: Int): CharacterItem = dao.getCharacterById(id).character.toDomain().apply {
+        origin = dao.getCharacterById(id).origin.toDomain()
+        location = dao.getCharacterById(id).location.toDomain()
     }
 
 
-
-    override suspend fun getAllFavoritesCharacter(): List<CharacterItem> {
-        val listCharacters = mutableListOf<CharacterItem>()
-
-        dao.getFavoritesCharacter().forEach {
-            val entity = it.character.toDomain()
-            entity.origin = it.origin.toDomain()
-            entity.location = it.location.toDomain()
-            listCharacters.add(entity)
+    override suspend fun getAllFavoritesCharacter(): List<CharacterItem> = dao.getFavoritesCharacter().map {
+        it.character.toDomain().apply {
+            origin = it.origin.toDomain()
+            location = it.location.toDomain()
         }
-        return listCharacters
     }
 
 
